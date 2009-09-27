7eb018cb16c689fa8ed6712dd11596187d615edc
Wyatt Greene <wyatt.greene@makemesustainable.com>
2009-09-27 18:26:12 -0400

    Added google analytics.

--- page.rb
+++ page.rb
@@ -28,6 +28,7 @@ class Page
             </form>
             #{results}
           </div>
+          #{google_analytics}
         </body>
       </html>
     }
@@ -88,6 +89,20 @@ class Page
           </ul>
         </div>
       </div>
+    }
+  end
+  
+  def google_analytics
+    %Q{
+      <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+      </script>
+      <script type="text/javascript">
+      try {
+      var pageTracker = _gat._getTracker("UA-6313299-16");
+      pageTracker._trackPageview();
+      } catch(err) {}</script>
     }
   end
 
